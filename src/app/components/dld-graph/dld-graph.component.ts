e4e1bb6ad370a148fb4bac2177f111760e1c8583
vyasbalimidi <vbalimi@capelit51748832.inedc.corpintra.net>
2018-03-27 11:06:07 +0530

    Upload CSV file and generate graph

--- src/app/components/dld-graph/dld-graph.component.ts
+++ src/app/components/dld-graph/dld-graph.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-dld-graph',
@@ -6,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dld-graph.component.css']
 })
 export class DldGraphComponent implements OnInit {
-  constructor() { }
+  fileRead: any
+  public axisX: number[] = [];
+  public dataSeries: any[] = [
+    { data: [], label: 'First Run' }
+  ];
 
-  ngOnInit() {
+  constructor(private spinner: NgxSpinnerService) { }
 
+  ngOnInit() { }
+
+  convertFile(csv: any) {
+    this.spinner.show()
+    this.fileRead = csv.target.files[0]
+
+    let reader = new FileReader()
+    reader.readAsText(this.fileRead)
+
+    reader.onload = (e) => {
+      let csv: string = reader.result
+      let allTextLines = csv.split(/\n/)
+      let axisX: number[] = []
+      let axisY: number[] = []
+
+      for (let i = 0; i < allTextLines.length; i++) {
+        let data = allTextLines[i].split(',');
+
+        axisX.push(Number.parseFloat(data[0]))
+        axisY.push(Number.parseFloat(data[1]))
+      }
+
+      this.axisX = axisX
+      setTimeout(() => {
+        this.dataSeries[0].data = axisY
+        this.dataSeries = this.dataSeries.slice()
+      });
+      this.spinner.hide()
+    }
   }
 }
